112d8322df792fe2a69f9d4b9c38c322d78b3e85
Hraday04 <146326419+Hraday04@users.noreply.github.com>
2024-10-20 00:14:59 +0530

    Add files via upload

---
+++ Prac 9-A.php
@@ -0,0 +1,10 @@
+<?php
+$n = 5; // number of rows
+
+for ($i = 0; $i < $n; $i++) {
+    for ($j = 0; $j <= $i; $j++) {
+        echo $i;
+    }
+    echo "<br>";
+}
+?>
---
+++ Prac 9-B.php
@@ -0,0 +1,14 @@
+<?php
+$array = [1,2,3,4,5,6,0,67];
+
+echo "Find Minimum & Maximum value <br>";
+
+$maxValue = max($array);
+$minValue = min($array);
+
+echo "Maximum Value: $maxValue\n<br>";
+echo "Minimum Value: $minValue\n";
+?>
+
+
+
